952695dc65506a1369996e63a4aa3d1b682f3205
Imon2537 <114398089+Imon2537@users.noreply.github.com>
2024-08-13 14:51:29 +0300

    Update BotCollection.js

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,13 +1,13 @@
 import React from "react";
 import BotCard from "./BotCard";
-// import BotSpecs from "./BotSpecs";
+
 
 function BotCollection({bots, handleClick, handleDelete}) {
-  // Your code here
+  
   return (
     <div className="ui four column grid">
       <div className="row">        
-        {/*...and here..*/}
+        
         Collection of all bots
         <div className="" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', gap: '5px'}}>
         {bots.map((bot)=> <BotCard
